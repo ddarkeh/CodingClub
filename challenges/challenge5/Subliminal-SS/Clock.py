e79c815bca7c133ad09eddfb1f259a58317b838b
Subliminal-SS <Jonathan.Turnock@outlook.com>
2017-01-08 20:23:51 +0000

    Completed Program

--- challenges/challenge5/Subliminal-SS/Clock.py
+++ challenges/challenge5/Subliminal-SS/Clock.py
@@ -1,5 +1,36 @@
 #! /usr/local/bin/python3
-#Ask The User for the Time Input
-v_input = input("What Time to Start From?")
+import re
+import time
 
-print("User Inputed Time: " + v_input)
+def f_FowardTime(v_InputTime):
+   l_TimeList = v_InputTime.split(":")
+   v_sec = int(l_TimeList[2])
+   v_min = int(l_TimeList[1])
+   v_hou = int(l_TimeList[0])
+   v_TotalSeconds = (v_hou * 3600) + (v_min * 60) + v_sec
+   v_TotalSeconds = v_TotalSeconds + 1
+   v_OutputTime = time.strftime('%H:%M:%S', time.gmtime(v_TotalSeconds))
+   return v_OutputTime
+
+#START OF MAIN PROGRAM
+v_InputCheck = False
+
+while (v_InputCheck == False):
+   v_Input = input("Enter the Time to Start From in HH24:MM:SS")
+   #v_Input = "23:50:59"
+   re_Input = re.compile("^[0-2][0-3]:[0-5][0-9]:[0-5][0-9]$")
+   result = re_Input.match(v_Input)
+   if result:
+       v_InputCheck = True
+   else:
+       v_InputCheck = False
+
+
+v_Time = v_Input
+
+print(v_Time)
+
+while True:
+   v_Time = f_FowardTime(v_Time)
+   print(v_Time)
+   time.sleep(1)
